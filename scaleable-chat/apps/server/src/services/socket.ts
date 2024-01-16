7ec40b79e83b5fb5a34e2c81693662d5a60065bc
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2024-01-16 23:52:29 +0530

    socketio with redis working, messages are synchrnous

--- scaleable-chat/apps/server/src/services/socket.ts
+++ scaleable-chat/apps/server/src/services/socket.ts
@@ -1,13 +1,17 @@
 import { Server } from 'socket.io';
 import Redis from 'ioredis';
 
-// const pub = new Redis({
-// 	host: 'redis-338a3f8b-project-a675.a.aivencloud.com',
-// 	port: '26872',
-// 	username: 'default',
-// 	password: 'AVNS_k3LboluwvQLDH4bbmcu',
-// });
-// const usb = new Redis();
+const pub = new Redis({
+	host: 'redis-338a3f8b-project-a675.a.aivencloud.com',
+	port: 26872,
+	password: 'AVNS_k3LboluwvQLDH4bbmcu',
+});
+
+const sub = new Redis({
+	host: 'redis-338a3f8b-project-a675.a.aivencloud.com',
+	port: 26872,
+	password: 'AVNS_k3LboluwvQLDH4bbmcu',
+});
 
 class SocketService {
 	private _io: Server;
@@ -19,6 +23,7 @@ class SocketService {
 				origin: '*',
 			},
 		});
+		sub.subscribe("MESSAGES");
 	}
 
 	public initListeners() {
@@ -29,8 +34,15 @@ class SocketService {
 
 			socket.on('event:message', async ({ message }: { message: string }) => {
 				console.log('New Message Rec.', message);
+				await pub.publish('MESSAGES', JSON.stringify({ message }));
 			});
 		});
+
+		sub.on('message', (channel, message) => {
+			if(channel === 'MESSAGES'){
+				io.emit("message", message);
+			}
+		})
 	}
 
 	get io() {
